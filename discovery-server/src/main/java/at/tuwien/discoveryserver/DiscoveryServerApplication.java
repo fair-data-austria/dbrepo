4e2203dcf3a4fb1c62161c6abb553d374f22b216
dasdemgo <e1029590@student.tuwien.ac.at>
2020-10-06 17:53:11 +0200

    created microservice projects

---
+++ discovery-server/src/main/java/at/tuwien/discoveryserver/DiscoveryServerApplication.java
@@ -0,0 +1,15 @@
+package at.tuwien.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+
+public class DiscoveryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServerApplication.class, args);
+    }
+
+}
