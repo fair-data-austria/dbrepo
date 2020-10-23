64cdf3ed90184b512154d9da644f9daf999fcdd2
Cornelia <cornelia.michlits@tuwien.ac.at>
2020-10-23 20:09:25 +0200

    implement service hopefully :)

---
+++ analyse-service/app.py
@@ -0,0 +1,42 @@
+import os
+import uuid
+from flask import Flask, flash, request, redirect, url_for, Response
+from werkzeug.utils import secure_filename
+from determine_dt import determine_datatypes
+
+UPLOAD_FOLDER = '.'
+ALLOWED_EXTENSIONS = {'csv'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+            ## check if csv TODO
+        if file:
+            filename = str(uuid.uuid1())
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            result = determine_datatypes(filename)
+            os.remove(filename)
+            return result, 200
+    else:
+            return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
