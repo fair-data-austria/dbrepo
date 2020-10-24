2d884a1fd8600899ac20bd90da49482de60452ce
dasdemgo <e1029590@student.tuwien.ac.at>
2020-10-25 01:14:13 +0200

    changed project names, created empty table service

---
+++ fda-table-service/src/main/java/at/tuwien/fdatableservice/FdaTableServiceApplication.java
@@ -0,0 +1,13 @@
+package at.tuwien.fdatableservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FdaTableServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FdaTableServiceApplication.class, args);
+    }
+
+}
