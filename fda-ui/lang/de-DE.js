86bdfc991add5ec49cbc329778bf66286990a3f9
Martin Weise <martin.weise@tuwien.ac.at>
2021-03-05 16:24:52 +0100

    added fda-ui

---
+++ fda-ui/lang/de-DE.js
@@ -0,0 +1,4 @@
+export default {
+  db_created_success: 'Datenbank wurde erfolgreich erstellt',
+  internal_server_error: 'Internal Server Error'
+}
---
+++ fda-ui/pages/tables.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h3>
+      Tables
+    </h3>
+    ...
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'Tables',
+  components: {
+  },
+  data () {
+    return {}
+  },
+  mounted () {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+</style>
