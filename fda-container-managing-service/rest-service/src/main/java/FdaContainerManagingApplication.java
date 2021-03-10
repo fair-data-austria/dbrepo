8a9dbf5d99ab98ec51b6de8cb4791bf18aeba2a4
Martin Weise <martin.weise@tuwien.ac.at>
2021-03-10 20:05:26 +0100

    structure now according to mavens standard

--- fda-container-managing-service/rest-service/src/main/java/FdaContainerManagingApplication.java
+++ fda-container-managing-service/rest-service/src/main/java/FdaContainerManagingApplication.java
@@ -1,12 +1,10 @@
-package rest;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "services.entities")
+@ComponentScan(basePackages = "entities")
 @EnableSwagger2
 public class FdaContainerManagingApplication {
 
