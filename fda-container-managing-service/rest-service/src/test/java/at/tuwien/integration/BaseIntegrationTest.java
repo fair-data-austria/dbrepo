997d0bf88592c212a61b0a2d0e2cbac7296b7996
Martin Weise <martin.weise@tuwien.ac.at>
2021-03-11 11:47:49 +0100

    added base tests

---
+++ fda-container-managing-service/rest-service/src/test/java/at/tuwien/integration/BaseIntegrationTest.java
@@ -0,0 +1,8 @@
+package at.tuwien.integration;
+
+import org.springframework.test.context.TestPropertySource;
+
+@TestPropertySource(locations = "classpath:application.properties")
+public abstract class BaseIntegrationTest {
+
+}
---
+++ fda-container-managing-service/rest-service/src/test/java/at/tuwien/integration/EndpointTest.java
@@ -0,0 +1,18 @@
+package at.tuwien.integration;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class EndpointTest extends BaseIntegrationTest {
+
+    @Test
+    public void contextLoads() {
+        //
+    }
+}
