94a450b8dc630d58417e44e8330cc273383dbc6b
Martin Weise <martin.weise@tuwien.ac.at>
2021-03-16 21:29:20 +0100

    finally swagger ui spin-up, there were undocumented dependencies

--- fda-container-managing-service/rest-service/src/main/java/at/tuwien/config/SwaggerConfig.java
+++ fda-container-managing-service/rest-service/src/main/java/at/tuwien/config/SwaggerConfig.java
@@ -2,21 +2,18 @@ package at.tuwien.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
 import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Collections;
-import java.util.HashSet;
-
-import static com.google.common.collect.Lists.newArrayList;
 
 @Configuration
+@EnableOpenApi
 public class SwaggerConfig {
 
     @Bean
@@ -38,7 +35,6 @@ public class SwaggerConfig {
                 "API license",
                 null,
                 Collections.emptyList());
-
 
     }
 
