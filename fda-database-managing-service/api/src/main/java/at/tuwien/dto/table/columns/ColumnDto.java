db7b6f238eff8ab4b331cd106e1255b3e4bf0850
Martin Weise <martin.weise@tuwien.ac.at>
2021-03-17 09:47:15 +0100

    added column definition, updated api definition

---
+++ fda-database-managing-service/api/src/main/java/at/tuwien/dto/table/columns/ColumnDto.java
@@ -0,0 +1,41 @@
+package at.tuwien.dto.table.columns;
+
+import io.swagger.v3.oas.annotations.Parameter;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ColumnDto {
+
+    @Parameter(name = "column type")
+    private ColumnTypeDto type;
+
+    @Parameter(name = "categories", description = "only categorical, derive totalCategories from array")
+    private String[] categories;
+
+    @Parameter(name = "categories", description = "only nominal and numerical")
+    private Double maxLength;
+
+    @Parameter(name = "categories", description = "only numerical")
+    private SiUnitDto siUnit;
+
+    @Parameter(name = "categories", description = "only numerical")
+    private Double min;
+
+    @Parameter(name = "categories", description = "only numerical")
+    private Double max;
+
+    @Parameter(name = "categories", description = "only numerical")
+    private Double mean;
+
+    @Parameter(name = "categories", description = "only numerical")
+    private Double median;
+
+    @Parameter(name = "categories", description = "only numerical")
+    private Double standardDeviation;
+
+    @Parameter(name = "categories", description = "only numerical")
+    private Object histogram;
+
+}
--- fda-database-managing-service/api/src/main/java/at/tuwien/dto/table/columns/RowDto.java
+++ fda-database-managing-service/api/src/main/java/at/tuwien/dto/table/columns/RowDto.java
@@ -5,8 +5,10 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class NominalColumnDto extends AbstractColumnDto {
+public class RowDto {
 
-    private Long maxLength;
+    private ColumnDto[] columns;
+
+    private String[] data;
 
 }
