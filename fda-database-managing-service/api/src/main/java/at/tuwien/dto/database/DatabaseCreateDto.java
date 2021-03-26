c613e0ee33a2a06a89ce7c5db37e7faf4e53bcfd
Martin Weise <martin.weise@tuwien.ac.at>
2021-03-26 15:43:56 +0100

    split the repo and tag

--- fda-database-managing-service/api/src/main/java/at/tuwien/dto/database/DatabaseCreateDto.java
+++ fda-database-managing-service/api/src/main/java/at/tuwien/dto/database/DatabaseCreateDto.java
@@ -18,42 +18,11 @@ public class DatabaseCreateDto {
     private String containerId;
 
     @NotBlank
-    @Size(min = 3)
     @Parameter(name = "database name")
     private String name;
 
     @NotBlank
     @Parameter(name = "database engine", example = "postgres")
     private String engine;
-
-    @NotBlank
-    @Size(min = 3)
-    @Parameter(name = "database owner")
-    private String owner;
-
-    @NotBlank
-    @Size(min = 3)
-    @Parameter(name = "database creator name")
-    private String creator;
-
-    @NotBlank
-    @Size(min = 3)
-    @Parameter(name = "database publisher name")
-    private String publisher;
-
-    @NotBlank
-    @Parameter(name = "database publication year", example = "2021")
-    private Instant publicationYear;
-
-    @NotBlank
-    @Parameter(name = "database resource type")
-    private String ResourceType;
-
-    @Parameter(name = "database description")
-    private String description;
-
-    @NotBlank
-    @Parameter(name = "database table definition scheme")
-    private TableDto[] tables;
 
 }
