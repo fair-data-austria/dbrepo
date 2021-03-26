b5c9e6ac167b2d618913faa2bc31dfc4eab53b6a
Martin Weise <martin.weise@tuwien.ac.at>
2021-03-26 13:42:50 +0100

    remove seeder

--- fda-container-managing-service/services/src/main/java/at/tuwien/mapper/ImageMapper.java
+++ fda-container-managing-service/services/src/main/java/at/tuwien/mapper/ImageMapper.java
@@ -1,17 +1,9 @@
 package at.tuwien.mapper;
 
-import at.tuwien.api.dto.IpAddressDto;
-import at.tuwien.api.dto.container.ContainerBriefDto;
-import at.tuwien.api.dto.container.ContainerCreateRequestDto;
-import at.tuwien.api.dto.container.ContainerDto;
 import at.tuwien.api.dto.image.ImageBriefDto;
 import at.tuwien.api.dto.image.ImageDto;
-import at.tuwien.entity.Architecture;
-import at.tuwien.entity.Container;
 import at.tuwien.entity.ContainerImage;
-import com.github.dockerjava.api.command.InspectContainerResponse;
 import com.github.dockerjava.api.command.InspectImageResponse;
-import com.github.dockerjava.api.model.NetworkSettings;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
@@ -25,7 +17,6 @@ public interface ImageMapper {
 
     @Mappings({
             @Mapping(source = "id", target = "hash"),
-            @Mapping(source = "arch", target = "architecture"),
     })
     ContainerImage inspectImageResponseToContainerImage(InspectImageResponse data);
 
