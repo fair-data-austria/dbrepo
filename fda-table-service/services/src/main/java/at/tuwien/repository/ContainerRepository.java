ae547fc00b3deaf6cd4ea6ff1399b205af12c7b5
Martin Weise <martin.weise@tuwien.ac.at>
2021-03-27 23:05:23 +0100

    remove containerId

--- fda-table-service/services/src/main/java/at/tuwien/repository/ContainerRepository.java
+++ fda-table-service/services/src/main/java/at/tuwien/repository/ContainerRepository.java
@@ -5,6 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ContainerRepository extends JpaRepository<Container, Long> {
 
-    Container findContainerByContainerId(String containerId);
-
 }
--- fda-table-service/services/src/main/java/at/tuwien/repository/DatabaseRepository.java
+++ fda-table-service/services/src/main/java/at/tuwien/repository/DatabaseRepository.java
@@ -6,6 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DatabaseRepository extends JpaRepository<Database, Long> {
-
     
 }
