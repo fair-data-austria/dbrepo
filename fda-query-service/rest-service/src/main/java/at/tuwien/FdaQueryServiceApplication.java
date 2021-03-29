5257559ce445be5b5650790e1b9888464d631718
Moritz Staudinger <e11777768@student.tuwien.ac.at>
2021-03-29 10:33:49 +0200

    Reconfigured Swagger

--- fda-query-service/rest-service/src/main/java/at/tuwien/FdaQueryServiceApplication.java
+++ fda-query-service/rest-service/src/main/java/at/tuwien/FdaQueryServiceApplication.java
@@ -1,14 +1,16 @@
-package at.tuwien.application;
+package at.tuwien;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
-@ComponentScan(basePackages = "at.tuwien")
-@EnableSwagger2
+@EntityScan(basePackages = "at.tuwien")
+@EnableOpenApi
 public class FdaQueryServiceApplication {
 
     public static void main(String[] args) {
