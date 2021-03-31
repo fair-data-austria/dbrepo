ce14cfbbacf92e3a626ef5987116a75d3731004b
Kirill Stytsenko <kirill@styts.com>
2021-03-31 12:31:05 +0200

    Add mock for database listing endpoint

--- fda-ui/nuxt.config.js
+++ fda-ui/nuxt.config.js
@@ -31,7 +31,8 @@ export default {
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
     { src: '~/plugins/toast', ssr: false }, // only client side
-    { src: '~/plugins/vuex-persist', ssr: false } // only client side
+    { src: '~/plugins/vuex-persist', ssr: false }, // only client side
+    { src: '~/plugins/mock' }
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
