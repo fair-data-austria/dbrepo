3e467cc013eaf9a82acde3ee56671beb40ee373a
Martin Weise <martin.weise@tuwien.ac.at>
2021-03-31 15:34:21 +0200

    more coherent naming of db + container service

--- fda-table-service/api/src/main/java/at/tuwien/dto/table/columns/RowDto.java
+++ fda-table-service/api/src/main/java/at/tuwien/dto/table/columns/RowDto.java
@@ -2,17 +2,20 @@ package at.tuwien.dto.table.columns;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
+@ToString
 public class RowDto {
 
-    @NotBlank
+    @NotNull
     private ColumnDto[] columns;
 
-    @NotBlank
+    @NotNull
     private String[] data;
 
 }
--- fda-table-service/api/src/main/java/at/tuwien/dto/table/columns/SiUnitDto.java
+++ fda-table-service/api/src/main/java/at/tuwien/dto/table/columns/SiUnitDto.java
@@ -1,5 +1,10 @@
 package at.tuwien.dto.table.columns;
 
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
 public enum SiUnitDto {
     SECOND, METER, KILOGRAM, AMPERE, KELVIN, MOLE, CANDELA
 }
