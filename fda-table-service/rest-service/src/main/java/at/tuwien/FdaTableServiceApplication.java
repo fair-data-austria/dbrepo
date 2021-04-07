4518f50b90423e9d651bf73979f65a4bfc049f98
Martin Weise <martin.weise@tuwien.ac.at>
2021-04-08 00:12:41 +0200

    not needed

--- fda-table-service/rest-service/src/main/java/at/tuwien/FdaTableServiceApplication.java
+++ fda-table-service/rest-service/src/main/java/at/tuwien/FdaTableServiceApplication.java
@@ -3,7 +3,6 @@ package at.tuwien;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -12,7 +11,6 @@ import springfox.documentation.oas.annotations.EnableOpenApi;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableDiscoveryClient
 @EnableOpenApi
 @EnableTransactionManagement
 @EnableJpaRepositories(basePackages = {"at.tuwien.repository"})
