ce6ab626d4f495c4dd8799233294c8c1e977f556
Kirill Stytsenko <kirill@styts.com>
2021-04-08 16:47:51 +0200

    Add DB index

---
+++ fda-ui/pages/db/_id/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      DB {{ $route.params.id }}
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  components: {},
+  mounted () {
+    console.log(this)
+  }
+}
+</script>
