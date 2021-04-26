e9ee60d422fff95feb95d58a24e3d1731c47cadd
Kirill Stytsenko <kirill@styts.com>
2021-04-26 15:30:47 +0200

    Display table data

--- fda-ui/pages/db/_db_id/tables/_table_id/index.vue
+++ fda-ui/pages/db/_db_id/tables/_table_id/index.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
     <h3>
-      Table Paginated Listing
+      {{ tableName }}
     </h3>
     <v-data-table
       :headers="headers"
       :items="rows"
+      :loading="loading"
       :items-per-page="10"
       class="elevation-1" />
   </div>
@@ -17,19 +18,41 @@ export default {
   },
   data () {
     return {
-      headers: [
-        { value: 'foo', text: 'Foo' },
-        {}
-      ],
-      rows: [
-        { foo: 'Bar' },
-        {}
-      ]
+      loading: true,
+      tableName: '',
+      headers: [],
+      rows: []
     }
   },
-  mounted () {
+  async mounted () {
+    await this.loadProperties()
+    this.loadData()
   },
   methods: {
+    async loadProperties () {
+      try {
+        const res = await this.$axios.get(`/api/tables/api/database/${this.$route.params.db_id}/table/${this.$route.params.table_id}`)
+        this.tableName = res.data.name
+        this.headers = res.data.columns.map((c) => {
+          return {
+            value: c.name,
+            text: c.name
+          }
+        })
+      } catch (err) {
+        this.$toast.error('Could not get table details.')
+        this.loading = false
+      }
+    },
+    async loadData () {
+      try {
+        const res = await this.$axios.get(`/api/tables/api/database/${this.$route.params.db_id}/table/${this.$route.params.table_id}/data`)
+        this.rows = res.data.Result
+      } catch (err) {
+        this.$toast.error('Could not load table data.')
+      }
+      this.loading = false
+    }
   }
 }
 </script>
