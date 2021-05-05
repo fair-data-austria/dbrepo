beb76038a7882d6453330f6710b3640e0272d1a5
Martin Weise <martin.weise@tuwien.ac.at>
2021-05-05 10:54:34 +0200

    properly test the container endpoint now
    Former-commit-id: a01547d2547c2bb121a3cc957fee8b582f77e30a

--- fda-container-service/services/src/main/java/at/tuwien/repository/ImageRepository.java
+++ fda-container-service/services/src/main/java/at/tuwien/repository/ImageRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ImageRepository extends JpaRepository<ContainerImage, Long> {
 
-    ContainerImage findByRepositoryAndTag(String repository, String tag);
+    Optional<ContainerImage> findByRepositoryAndTag(String repository, String tag);
 
 }
