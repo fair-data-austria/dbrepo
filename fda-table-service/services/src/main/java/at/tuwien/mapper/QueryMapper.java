819565f970baf13cb80430613182f9e63457d730
Martin Weise <martin.weise@tuwien.ac.at>
2021-05-11 13:32:44 +0200

    compiles on laptop
    Former-commit-id: eaf9caf5f0ee5cd47ec06ffa93e74d6fb6db5a0a

--- fda-table-service/services/src/main/java/at/tuwien/mapper/QueryMapper.java
+++ fda-table-service/services/src/main/java/at/tuwien/mapper/QueryMapper.java
@@ -1,11 +1,10 @@
 package at.tuwien.mapper;
 
 import at.tuwien.api.database.query.QueryResultDto;
-import at.tuwien.entities.database.query.QueryResult;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface QueryMapper {
 
-    QueryResultDto queryResultToQueryResultDto(QueryResult queryResult);
+    QueryResultDto queryResultToQueryResultDto(QueryResultDto queryResult);
 }
