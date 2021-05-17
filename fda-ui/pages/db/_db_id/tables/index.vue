ba1c3fc8917030154d728012feeb1871d68dc001
Kirill Stytsenko <kirill@styts.com>
2021-05-17 11:13:04 +0200

    Basic template for table_from_csv view (incl. stepper)

--- fda-ui/pages/db/_db_id/tables/index.vue
+++ fda-ui/pages/db/_db_id/tables/index.vue
@@ -5,6 +5,13 @@
     </h3>
     <TableList />
     <TableCreate />
+    <v-card class="mt-1">
+      <v-card-text>
+        <nuxt-link class="table_from_csv" :to="`/db/${$route.params.db_id}/tables/table_from_csv`">
+          Create table from CSV file
+        </nuxt-link>
+      </v-card-text>
+    </v-card>
   </div>
 </template>
 <script>
@@ -27,5 +34,8 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+a.table_from_csv {
+  font-size: 14pt;
+}
 </style>
