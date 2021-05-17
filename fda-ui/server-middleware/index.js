91dea991c823496461a8fc686c6b7768487ec8bf
Kirill Stytsenko <kirill@styts.com>
2021-05-17 13:05:22 +0200

    Get column types from analyse service

--- fda-ui/server-middleware/index.js
+++ fda-ui/server-middleware/index.js
@@ -1,13 +1,43 @@
-export default function (req, res, next) {
-  // req is the Node.js http request object
-  console.log(req.url)
+// const bodyParser = require('body-parser')
+const app = require('express')()
+const multer = require('multer')
+const upload = multer({ dest: '/tmp' })
+const fetch = require('node-fetch')
 
-  // res is the Node.js http response object
-
-  // next is a function to call to invoke the next middleware
-  // Don't forget to call next at the end if your middleware is not an endpoint!
-  // next()
-  console.log(res)
-
-  return res.json({ hi: 'foo' })
+// TODO extend me
+const colTypeMap = {
+  Integer: 'NUMBER',
+  String: 'TEXT'
 }
+
+app.post('/table_from_csv', upload.single('file'), async (req, res) => {
+  const { file } = req
+  const { path } = file
+
+  // send path to analyse service
+  let analysis
+  try {
+    analysis = await fetch(`${process.env.API_ANALYSE}/datatypesbypath?filepath=${path}`)
+    analysis = await analysis.json()
+  } catch (error) {
+    res.json({ success: false, error })
+  }
+  console.log('Analysis', analysis)
+
+  // map `determine_dt` column types to ours
+  // e.g. "Integer" -> "NUMBER"
+  let entries = Object.entries(analysis.columns)
+  console.log(entries)
+  entries = entries.map(([k, v]) => {
+    if (colTypeMap[v]) {
+      v = colTypeMap[v]
+    }
+    return [k, v]
+  })
+
+  console.log(entries)
+
+  res.json({ success: true, file, columns: entries })
+})
+
+module.exports = app
