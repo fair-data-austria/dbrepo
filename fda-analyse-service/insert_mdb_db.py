74d4953a8e0afeb7c465c158e7dbc4b01684e32e
Cornelia Michlits <cornelia.michlits@tuwien.ac.at>
2021-05-17 14:29:34 +0200

    #24 entity mdb_databases

---
+++ fda-analyse-service/insert_mdb_db.py
@@ -0,0 +1,25 @@
+from psycopg2 import connect 
+import requests
+import json 
+
+def insert_mdb_db(dbid, resourcetype, description, publisher):
+    # Connect to Meta database 
+    try: 
+        conn=connect(
+        dbname="fda", 
+        user = "postgres",
+        host = "fda-metadata-db", 
+        password = "postgres"
+        )
+    
+        cursor = conn.cursor() 
+    
+        # Insert into Table DATABASES 
+        cursor.execute("""Update mdb_databases set (ResourceType,Description,Publisher,last_modified) = 
+        (%s,%s,%s,current_timestamp)
+        where id=%s;""", (resourcetype, description, publisher,dbid,))
+        conn.commit()
+        conn.close()
+    except Exception as e: 
+        print("Error while connecting to mdb",e)
+        
