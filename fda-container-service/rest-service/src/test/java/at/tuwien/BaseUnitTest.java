0444fcf6a5d88810a4199de6b3b77a6b00b27e4c
Martin Weise <martin.weise@tuwien.ac.at>
2021-05-23 18:31:17 +0200

    container service code coverage is now 84%

--- fda-container-service/rest-service/src/test/java/at/tuwien/BaseUnitTest.java
+++ fda-container-service/rest-service/src/test/java/at/tuwien/BaseUnitTest.java
@@ -2,8 +2,10 @@ package at.tuwien;
 
 import at.tuwien.api.container.ContainerBriefDto;
 import at.tuwien.api.container.ContainerDto;
+import at.tuwien.api.container.image.ImageBriefDto;
 import at.tuwien.api.container.image.ImageDto;
 import at.tuwien.api.container.image.ImageEnvItemDto;
+import at.tuwien.api.container.network.IpAddressDto;
 import at.tuwien.entities.container.Container;
 import at.tuwien.entities.container.image.ContainerImage;
 import at.tuwien.entities.container.image.ContainerImageEnvironmentItem;
@@ -12,6 +14,7 @@ import com.github.dockerjava.api.command.CreateContainerResponse;
 import com.github.dockerjava.core.command.CreateContainerCmdImpl;
 import org.springframework.test.context.TestPropertySource;
 
+import java.math.BigInteger;
 import java.time.Instant;
 import java.util.List;
 
@@ -19,14 +22,16 @@ import static java.time.temporal.ChronoUnit.DAYS;
 import static java.time.temporal.ChronoUnit.HOURS;
 
 @TestPropertySource(locations = "classpath:application.properties")
-public abstract class BaseIntegrationTest {
+public abstract class BaseUnitTest {
 
-    public final String IMAGE_1_REPOSITORY = "postgres";
-    public final String IMAGE_1_TAG = "13-alpine";
-    public final String IMAGE_1_HASH = "83b40f2726e5";
-    public final Integer IMAGE_1_PORT = 5432;
-    public final Instant IMAGE_1_BUILT = Instant.now().minus(40, HOURS);
-    public final List<ContainerImageEnvironmentItem> IMAGE_1_ENV = List.of(ContainerImageEnvironmentItem.builder()
+    public final static Long IMAGE_1_ID = 1L;
+    public final static String IMAGE_1_REPOSITORY = "postgres";
+    public final static String IMAGE_1_TAG = "13-alpine";
+    public final static String IMAGE_1_HASH = "83b40f2726e5";
+    public final static Integer IMAGE_1_PORT = 5432;
+    public final static Long IMAGE_1_SIZE = 12000L;
+    public final static Instant IMAGE_1_BUILT = Instant.now().minus(40, HOURS);
+    public final static List<ContainerImageEnvironmentItem> IMAGE_1_ENV = List.of(ContainerImageEnvironmentItem.builder()
                     .key("POSTGRES_USER")
                     .value("postgres")
                     .build(),
@@ -34,7 +39,7 @@ public abstract class BaseIntegrationTest {
                     .key("POSTGRES_PASSWORD")
                     .value("postgres")
                     .build());
-    public final ImageEnvItemDto[] IMAGE_1_ENV_DTO = List.of(ImageEnvItemDto.builder()
+    public final static ImageEnvItemDto[] IMAGE_1_ENV_DTO = List.of(ImageEnvItemDto.builder()
                     .key("POSTGRES_USER")
                     .value("postgres")
                     .build(),
@@ -44,12 +49,13 @@ public abstract class BaseIntegrationTest {
                     .build())
             .toArray(new ImageEnvItemDto[0]);
 
-    public final String IMAGE_2_REPOSITORY = "redis";
-    public final String IMAGE_2_TAG = "latest";
-    public final String IMAGE_2_HASH = "f877e80bb9ef";
-    public final Integer IMAGE_2_PORT = 6379;
-    public final Instant IMAGE_2_BUILT = Instant.now().minus(9, DAYS);
-    public final List<ContainerImageEnvironmentItem> IMAGE_2_ENV = List.of(ContainerImageEnvironmentItem.builder()
+    public final static String IMAGE_2_REPOSITORY = "redis";
+    public final static String IMAGE_2_TAG = "latest";
+    public final static String IMAGE_2_HASH = "f877e80bb9ef";
+    public final static Integer IMAGE_2_PORT = 6379;
+    public final static Long IMAGE_2_SIZE = 24000L;
+    public final static Instant IMAGE_2_BUILT = Instant.now().minus(9, DAYS);
+    public final static List<ContainerImageEnvironmentItem> IMAGE_2_ENV = List.of(ContainerImageEnvironmentItem.builder()
                     .key("POSTGRES_USER")
                     .value("postgres")
                     .build(),
@@ -57,7 +63,7 @@ public abstract class BaseIntegrationTest {
                     .key("POSTGRES_PASSWORD")
                     .value("postgres")
                     .build());
-    public final ImageEnvItemDto[] IMAGE_2_ENV_DTO = List.of(ImageEnvItemDto.builder()
+    public final static ImageEnvItemDto[] IMAGE_2_ENV_DTO = List.of(ImageEnvItemDto.builder()
                     .key("POSTGRES_USER")
                     .value("postgres")
                     .build(),
@@ -67,105 +73,124 @@ public abstract class BaseIntegrationTest {
                     .build())
             .toArray(new ImageEnvItemDto[0]);
 
-    public final ContainerImage IMAGE_1 = ContainerImage.builder()
+    public final static ContainerImage IMAGE_1 = ContainerImage.builder()
+            .id(IMAGE_1_ID)
             .repository(IMAGE_1_REPOSITORY)
             .tag(IMAGE_1_TAG)
             .hash(IMAGE_1_HASH)
             .compiled(IMAGE_1_BUILT)
+            .size(IMAGE_1_SIZE)
             .environment(IMAGE_1_ENV)
             .defaultPort(IMAGE_1_PORT)
             .build();
 
-    public final ImageDto IMAGE_1_DTO = ImageDto.builder()
+    public final static ImageDto IMAGE_1_DTO = ImageDto.builder()
             .repository(IMAGE_1_REPOSITORY)
             .tag(IMAGE_1_TAG)
             .hash(IMAGE_1_HASH)
             .compiled(IMAGE_1_BUILT)
+            .size(BigInteger.valueOf(IMAGE_1_SIZE))
             .environment(IMAGE_1_ENV_DTO)
             .defaultPort(IMAGE_1_PORT)
             .build();
 
-    public final ContainerImage IMAGE_2 = ContainerImage.builder()
+    public final static ContainerImage IMAGE_2 = ContainerImage.builder()
             .repository(IMAGE_2_REPOSITORY)
             .tag(IMAGE_2_TAG)
             .hash(IMAGE_2_HASH)
             .compiled(IMAGE_2_BUILT)
+            .size(IMAGE_2_SIZE)
             .environment(IMAGE_2_ENV)
             .defaultPort(IMAGE_2_PORT)
             .build();
 
-    public final ImageDto IMAGE_2_DTO = ImageDto.builder()
+    public final static ImageDto IMAGE_2_DTO = ImageDto.builder()
             .repository(IMAGE_2_REPOSITORY)
             .tag(IMAGE_2_TAG)
             .hash(IMAGE_2_HASH)
+            .size(BigInteger.valueOf(IMAGE_2_SIZE))
             .compiled(IMAGE_2_BUILT)
             .environment(IMAGE_2_ENV_DTO)
             .defaultPort(IMAGE_2_PORT)
             .build();
 
-    public final Long CONTAINER_1_ID = 1L;
-    public final String CONTAINER_1_HASH = "deadbeef";
-    public final ContainerImage CONTAINER_1_IMAGE = IMAGE_1;
-    public final String CONTAINER_1_NAME = "u01";
-    public final String CONTAINER_1_INTERNALNAME = "u01";
-    public final String CONTAINER_1_DATABASE = "univie";
-    public final String CONTAINER_1_IP = "231.145.98.83";
-    public final Instant CONTAINER_1_CREATED = Instant.now().minus(1, HOURS);
+    public final static Long CONTAINER_1_ID = 1L;
+    public final static String CONTAINER_1_HASH = "deadbeef";
+    public final static ContainerImage CONTAINER_1_IMAGE = IMAGE_1;
+    public final static String CONTAINER_1_NAME = "u01";
+    public final static String CONTAINER_1_INTERNALNAME = "u01";
+    public final static String CONTAINER_1_DATABASE = "univie";
+    public final static String CONTAINER_1_IP = "231.145.98.83";
+    public final static Instant CONTAINER_1_CREATED = Instant.now().minus(1, HOURS);
 
-    public final Long CONTAINER_2_ID = 2L;
-    public final String CONTAINER_2_HASH = "0ff1ce";
-    public final ContainerImage CONTAINER_2_IMAGE = IMAGE_2;
-    public final String CONTAINER_2_NAME = "t01";
-    public final String CONTAINER_2_INTERNALNAME = "t01";
-    public final String CONTAINER_2_DATABASE = "tuw";
-    public final String CONTAINER_2_IP = "233.145.99.83";
-    public final Instant CONTAINER_2_CREATED = Instant.now().minus(1, HOURS);
+    public final static Long CONTAINER_2_ID = 2L;
+    public final static String CONTAINER_2_HASH = "0ff1ce";
+    public final static ContainerImage CONTAINER_2_IMAGE = IMAGE_2;
+    public final static String CONTAINER_2_NAME = "t01";
+    public final static String CONTAINER_2_INTERNALNAME = "t01";
+    public final static String CONTAINER_2_DATABASE = "tuw";
+    public final static String CONTAINER_2_IP = "233.145.99.83";
+    public final static Instant CONTAINER_2_CREATED = Instant.now().minus(1, HOURS);
 
-    public final Container CONTAINER_1 = Container.builder()
+    public final static Container CONTAINER_1 = Container.builder()
             .id(CONTAINER_1_ID)
             .name(CONTAINER_1_NAME)
+            .internalName(CONTAINER_1_INTERNALNAME)
             .image(CONTAINER_1_IMAGE)
             .hash(CONTAINER_1_HASH)
             .containerCreated(CONTAINER_1_CREATED)
             .build();
 
-    public final Container CONTAINER_2 = Container.builder()
+    public final static Container CONTAINER_2 = Container.builder()
             .id(CONTAINER_2_ID)
             .name(CONTAINER_2_NAME)
+            .internalName(CONTAINER_2_INTERNALNAME)
             .image(CONTAINER_2_IMAGE)
             .hash(CONTAINER_2_HASH)
             .containerCreated(CONTAINER_2_CREATED)
             .build();
 
-    public final ContainerDto CONTAINER_1_DTO = ContainerDto.builder()
+    public final static ContainerDto CONTAINER_1_DTO = ContainerDto.builder()
             .id(CONTAINER_1_ID)
             .name(CONTAINER_1_NAME)
             .image(IMAGE_1_DTO)
             .hash(CONTAINER_1_HASH)
+            .ipAddress(IpAddressDto.builder()
+                    .ipv4(CONTAINER_1_IP)
+                    .build())
             .created(CONTAINER_1_CREATED)
             .build();
 
-    public final ContainerBriefDto CONTAINER_1_BRIEF_DTO = ContainerBriefDto.builder()
+    public final static ContainerBriefDto CONTAINER_1_BRIEF_DTO = ContainerBriefDto.builder()
             .id(CONTAINER_1_ID)
             .name(CONTAINER_1_NAME)
             .internalName(CONTAINER_1_INTERNALNAME)
             .hash(CONTAINER_1_HASH)
             .build();
 
-    public final ContainerDto CONTAINER_2_DTO = ContainerDto.builder()
+    public final static ImageBriefDto IMAGE_1_BRIEFDTO = ImageBriefDto.builder()
+            .id(IMAGE_1_ID)
+            .repository(IMAGE_1_REPOSITORY)
+            .tag(IMAGE_1_TAG)
+            .build();
+
+    public final static ContainerDto CONTAINER_2_DTO = ContainerDto.builder()
             .id(CONTAINER_2_ID)
             .name(CONTAINER_2_NAME)
             .image(IMAGE_2_DTO)
             .hash(CONTAINER_2_HASH)
+            .ipAddress(IpAddressDto.builder()
+                    .ipv4(CONTAINER_2_IP)
+                    .build())
             .created(CONTAINER_2_CREATED)
             .build();
 
-    public final ContainerBriefDto CONTAINER_2_BRIEF_DTO = ContainerBriefDto.builder()
+    public final static ContainerBriefDto CONTAINER_2_BRIEF_DTO = ContainerBriefDto.builder()
             .id(CONTAINER_2_ID)
             .name(CONTAINER_2_NAME)
             .internalName(CONTAINER_2_INTERNALNAME)
             .hash(CONTAINER_2_HASH)
             .build();
 
-    public final CreateContainerResponse CONTAINER_1_RESPONSE = new CreateContainerResponse();
+    public final static CreateContainerResponse CONTAINER_1_RESPONSE = new CreateContainerResponse();
 }
---
+++ fda-container-service/rest-service/src/test/java/at/tuwien/endpoint/ImageEndpointUnitTest.java
@@ -0,0 +1,184 @@
+package at.tuwien.endpoint;
+
+import at.tuwien.BaseUnitTest;
+import at.tuwien.api.container.*;
+import at.tuwien.api.container.image.ImageBriefDto;
+import at.tuwien.api.container.image.ImageChangeDto;
+import at.tuwien.api.container.image.ImageCreateDto;
+import at.tuwien.api.container.image.ImageDto;
+import at.tuwien.endpoints.ContainerEndpoint;
+import at.tuwien.endpoints.ImageEndpoint;
+import at.tuwien.entities.container.image.ContainerImage;
+import at.tuwien.exception.*;
+import at.tuwien.repository.ImageRepository;
+import at.tuwien.service.ContainerService;
+import at.tuwien.service.ImageService;
+import com.github.dockerjava.api.DockerClient;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.persistence.EntityNotFoundException;
+import java.awt.*;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class ImageEndpointUnitTest extends BaseUnitTest {
+
+    @MockBean
+    private ImageService imageService;
+
+    @MockBean
+    private ImageRepository imageRepository;
+
+    @MockBean
+    private DockerClient dockerClient;
+
+    @Autowired
+    private ImageEndpoint imageEndpoint;
+
+    @Test
+    public void findAll_succeeds() {
+        when(imageService.getAll())
+                .thenReturn(List.of(IMAGE_1));
+
+        /* test */
+        final ResponseEntity<List<ImageBriefDto>> response = imageEndpoint.findAll();
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(1, Objects.requireNonNull(response.getBody()).size());
+    }
+
+    @Test
+    public void create_succeeds() {
+        final ImageCreateDto request = ImageCreateDto.builder()
+                .repository(IMAGE_1_REPOSITORY)
+                .tag(IMAGE_1_TAG)
+                .defaultPort(IMAGE_1_PORT)
+                .environment(IMAGE_1_ENV_DTO)
+                .build();
+        when(imageService.create(request))
+                .thenReturn(CONTAINER_1_IMAGE);
+
+        /* test */
+        final ResponseEntity<ImageDto> response = imageEndpoint.create(request);
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertEquals(IMAGE_1_REPOSITORY, Objects.requireNonNull(response.getBody()).getRepository());
+        assertEquals(IMAGE_1_TAG, Objects.requireNonNull(response.getBody()).getTag());
+    }
+
+    @Test
+    public void create_duplicate_fails() {
+        final ImageCreateDto request = ImageCreateDto.builder()
+                .repository(IMAGE_1_REPOSITORY)
+                .tag(IMAGE_1_TAG)
+                .defaultPort(IMAGE_1_PORT)
+                .environment(IMAGE_1_ENV_DTO)
+                .build();
+        given(imageService.create(request))
+                .willAnswer(invocation -> {
+                    throw new ImageAlreadyExistsException("duplicate");
+                });
+
+        /* test */
+        assertThrows(ImageAlreadyExistsException.class, () -> {
+            imageEndpoint.create(request);
+        });
+    }
+
+    @Test
+    public void create_notExists_fails() {
+        final ImageCreateDto request = ImageCreateDto.builder()
+                .repository(IMAGE_1_REPOSITORY)
+                .tag(IMAGE_1_TAG)
+                .defaultPort(IMAGE_1_PORT)
+                .environment(IMAGE_1_ENV_DTO)
+                .build();
+        given(imageService.create(request))
+                .willAnswer(invocation -> {
+                    throw new ImageNotFoundException("not existing in docker hub");
+                });
+
+        /* test */
+        assertThrows(ImageNotFoundException.class, () -> {
+            imageEndpoint.create(request);
+        });
+    }
+
+    @Test
+    public void findById_succeeds() {
+        when(imageService.getById(IMAGE_1_ID))
+                .thenReturn(CONTAINER_1_IMAGE);
+
+        /* test */
+        final ResponseEntity<ImageDto> response = imageEndpoint.findById(IMAGE_1_ID);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(IMAGE_1_HASH, Objects.requireNonNull(response.getBody()).getHash());
+    }
+
+    @Test
+    public void findById_notFound_fails() {
+        given(imageService.getById(IMAGE_1_ID))
+                .willAnswer(invocation -> {
+                    throw new ImageNotFoundException("not existing in docker hub");
+                });
+
+        /* test */
+        assertThrows(ImageNotFoundException.class, () -> {
+            imageEndpoint.findById(IMAGE_1_ID);
+        });
+    }
+
+    @Test
+    public void delete_fails() {
+        doThrow(new ImageNotFoundException("not found"))
+                .when(imageService)
+                .delete(IMAGE_1_ID);
+
+        /* test */
+        assertThrows(ImageNotFoundException.class, () -> {
+            imageEndpoint.delete(IMAGE_1_ID);
+        });
+    }
+
+    @Test
+    public void update_succeeds() {
+        final ImageChangeDto request = ImageChangeDto.builder()
+                .defaultPort(1111)
+                .build();
+
+        /* test */
+        final ResponseEntity<ImageDto> response = imageEndpoint.update(IMAGE_1_ID, request);
+        assertEquals(HttpStatus.ACCEPTED, response.getStatusCode());
+    }
+
+    @Test
+    public void update_notFound_fails() {
+        final ImageChangeDto request = ImageChangeDto.builder()
+                .defaultPort(1111)
+                .environment(IMAGE_1_ENV_DTO)
+                .build();
+        given(imageService.update(IMAGE_1_ID, request))
+                .willAnswer(invocation -> {
+                    throw new ImageNotFoundException("not existing in docker hub");
+                });
+
+        /* test */
+        assertThrows(ImageNotFoundException.class, () -> {
+            imageEndpoint.update(IMAGE_1_ID, request);
+        });
+    }
+}
