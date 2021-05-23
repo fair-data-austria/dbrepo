7fd676f48c4788558e7c4fe32c4951430f255ad4
Martin Weise <martin.weise@tuwien.ac.at>
2021-05-23 14:42:10 +0200

    added sample tests to generate reports
    Former-commit-id: f2f7665c47488ed912d0d1112df0a225df272856

---
+++ fda-query-service/rest-service/src/test/java/at/tuwien/endpoint/EndpointUnitTest.java
@@ -0,0 +1,17 @@
+package at.tuwien.endpoint;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class EndpointUnitTest {
+
+    @Test
+    public void contextLoads() {
+        //
+    }
+
+}
