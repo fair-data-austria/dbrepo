2ef25f7fa6493fa9bb5d6c6d2d5a7d60767aca05
Martin Weise <martin.weise@tuwien.ac.at>
2021-05-23 21:23:36 +0200

    postgres integration test mocks
    Former-commit-id: 7a65eeab6c02af334bd86014e00a7561ef38ff60

--- fda-table-service/rest-service/src/test/java/at/tuwien/service/PostgresServiceIntegrationTest.java
+++ fda-table-service/rest-service/src/test/java/at/tuwien/service/PostgresServiceIntegrationTest.java
@@ -1,4 +1,96 @@
 package at.tuwien.service;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
 public class PostgresServiceIntegrationTest {
+
+    @Autowired
+    private TableService tableService;
+
+    @Test
+    public void createTable_succeeds() {
+
+    }
+
+    @Test
+    public void createTable_fails() {
+
+    }
+
+    @Test
+    public void insertIntoTable_succeeds() {
+
+    }
+
+    @Test
+    public void insertIntoTable_noConnection_fails() {
+
+    }
+
+    @Test
+    public void insertIntoTable_noSql_fails() {
+
+    }
+
+    @Test
+    public void getAllRows_succeeds() {
+
+    }
+
+    @Test
+    public void getAllRows_noConnection_fails() {
+
+    }
+
+    @Test
+    public void getAllRows_noSql_fails() {
+
+    }
+
+    @Test
+    public void getCreateTableStatement_succeeds() {
+
+    }
+
+    @Test
+    public void getCreateTableStatement_noSql_fails() {
+
+    }
+
+    @Test
+    public void inserStatement_succeeds() {
+
+    }
+
+    @Test
+    public void deleteTable_succeeds() {
+
+    }
+
+    @Test
+    public void deleteTable_noConnection_fails() {
+
+    }
+
+    @Test
+    public void deleteTable_noSql_fails() {
+
+    }
+
+    @Test
+    public void getDeleteStatement_succeeds() {
+
+    }
+
+    @Test
+    public void getDeleteStatement_fails() {
+
+    }
+
 }
