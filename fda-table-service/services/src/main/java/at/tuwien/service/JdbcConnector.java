c55fa117d9cd84bbb7c3774a09674afff9544d2f
Martin Weise <martin.weise@tuwien.ac.at>
2021-05-25 13:39:47 +0200

    add integration test for postgres service

--- fda-table-service/services/src/main/java/at/tuwien/service/JdbcConnector.java
+++ fda-table-service/services/src/main/java/at/tuwien/service/JdbcConnector.java
@@ -3,6 +3,7 @@ package at.tuwien.service;
 
 import at.tuwien.api.database.table.TableCreateDto;
 import at.tuwien.entities.database.table.Table;
+import at.tuwien.exception.DataProcessingException;
 
 import java.sql.*;
 import java.util.List;
@@ -15,9 +16,9 @@ public abstract class JdbcConnector {
         return DriverManager.getConnection(url, properties);
     }
 
-    abstract PreparedStatement getCreateTableStatement(Connection connection, TableCreateDto createDto) throws SQLException;
+    abstract PreparedStatement getCreateTableStatement(Connection connection, TableCreateDto createDto) throws DataProcessingException;
 
     abstract String insertStatement(List<Map<String, Object>> processedData, Table t, List<String> headers);
 
-    abstract PreparedStatement getDeleteStatement(Connection connection, Table table) throws SQLException;
+    abstract PreparedStatement getDeleteStatement(Connection connection, Table table) throws DataProcessingException;
 }
