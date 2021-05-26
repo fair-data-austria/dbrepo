80daa517f751b9b42c6a950753ff240112028e1d
Kirill Stytsenko <kirill@styts.com>
2021-05-26 12:33:32 +0200

    Stub for QueryBuilder component
    Former-commit-id: b77c4d19bcda31c24a6ef01a97804520ecdb135f

---
+++ fda-ui/components/QueryBuilder.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>QB</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  mounted () {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
