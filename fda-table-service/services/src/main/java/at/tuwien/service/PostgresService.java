2bd584bf823fe0b9df6f1bc6b9e5ae574d5e4170
Martin Weise <martin.weise@tuwien.ac.at>
2021-05-26 18:04:51 +0200

    refactored the code a bit to better transform it to hibernate model
    Former-commit-id: 57ad33f9361a4df7805844ca73cb977e54827807

--- fda-table-service/services/src/main/java/at/tuwien/service/PostgresService.java
+++ fda-table-service/services/src/main/java/at/tuwien/service/PostgresService.java
@@ -2,7 +2,6 @@ package at.tuwien.service;
 
 import at.tuwien.api.database.query.QueryResultDto;
 import at.tuwien.api.database.table.TableCreateDto;
-import at.tuwien.api.database.table.columns.ColumnCreateDto;
 import at.tuwien.api.database.table.columns.ColumnTypeDto;
 import at.tuwien.entities.database.Database;
 import at.tuwien.entities.database.table.Table;
@@ -10,9 +9,7 @@ import at.tuwien.entities.database.table.columns.TableColumn;
 import at.tuwien.exception.DataProcessingException;
 import at.tuwien.exception.DatabaseConnectionException;
 import at.tuwien.exception.TableMalformedException;
-import at.tuwien.mapper.PostgresTableMapper;
 import at.tuwien.mapper.TableMapper;
-import at.tuwien.repository.TableRepository;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,23 +22,20 @@ import java.util.*;
 
 @Log4j2
 @Service
-public class PostgresService extends JdbcConnector {
+public class PostgresService extends JdbcConnector implements ContainerDatabaseService {
 
     private final Properties postgresProperties;
-    private final TableRepository tableRepository;
     private final TableMapper tableMapper;
-    private final PostgresTableMapper postgresTableMapper;
 
     @Autowired
-    protected PostgresService(Properties postgresProperties, TableRepository tableRepository,
-                              TableMapper tableMapper, PostgresTableMapper postgresTableMapper) {
+    protected PostgresService(Properties postgresProperties, TableMapper tableMapper) {
+        super(tableMapper);
         this.postgresProperties = postgresProperties;
-        this.tableRepository = tableRepository;
         this.tableMapper = tableMapper;
-        this.postgresTableMapper = postgresTableMapper;
     }
 
-    private Connection getConnection(Database database) throws DatabaseConnectionException {
+    @Override
+    public Connection getConnection(Database database) throws DatabaseConnectionException {
         Connection connection;
         final String URL = "jdbc:postgresql://" + database.getContainer().getInternalName() + ":"
                 + database.getContainer().getImage().getDefaultPort() + "/" + database.getInternalName();
@@ -54,6 +48,7 @@ public class PostgresService extends JdbcConnector {
         return connection;
     }
 
+    @Override
     public void createTable(Database database, TableCreateDto createDto) throws DatabaseConnectionException, TableMalformedException, DataProcessingException {
         try {
             final PreparedStatement statement = getCreateTableStatement(getConnection(database), createDto);
@@ -64,12 +59,12 @@ public class PostgresService extends JdbcConnector {
         }
     }
 
-    public QueryResultDto insertIntoTable(Database database, Table t, List<Map<String, Object>> processedData, List<String> headers) throws DatabaseConnectionException, DataProcessingException {
+    @Override
+    public QueryResultDto insertIntoTable(Database database, Table table, List<Map<String, Object>> processedData, List<String> headers) throws DatabaseConnectionException, DataProcessingException {
         try {
-            Connection connection = getConnection(database);
-            PreparedStatement statement = connection.prepareStatement(insertStatement(processedData, t, headers));
+            final PreparedStatement statement = getInsertStatement(getConnection(database), processedData, table, headers);
             statement.execute();
-            return getAllRows(database, t);
+            return getAllRows(database, table);
         } catch (DatabaseConnectionException e) {
             log.error("Problem with connecting to the database while selecting from query store: {}", e.getMessage());
             throw new DatabaseConnectionException("database connection problem with query store", e);
@@ -79,13 +74,7 @@ public class PostgresService extends JdbcConnector {
         }
     }
 
-    /**
-     * FIXME IN Sprint 2
-     *
-     * @param database
-     * @param t
-     * @return
-     */
+    @Override
     public QueryResultDto getAllRows(Database database, Table t) throws DatabaseConnectionException, DataProcessingException {
         try {
             Connection connection = getConnection(database);
@@ -141,17 +130,8 @@ public class PostgresService extends JdbcConnector {
         }
     }
 
-    /**
-     * FIXME THIS IS REMOVED IN SPRINT 2
-     * Very weird ordering of arguments in the processedData for-for loop
-     * Why not use PostgreSQL COPY statement?
-     *
-     * @param processedData
-     * @param t
-     * @return
-     */
     @Override
-    public String insertStatement(List<Map<String, Object>> processedData, Table t, List<String> headers) {
+    public PreparedStatement getInsertStatement(Connection connection, List<Map<String, Object>> processedData, Table t, List<String> headers) throws DataProcessingException {
         log.debug("insert table name: {}", t.getInternalName());
         StringBuilder queryBuilder = new StringBuilder()
                 .append("INSERT INTO ")
@@ -182,9 +162,15 @@ public class PostgresService extends JdbcConnector {
         queryBuilder.deleteCharAt(queryBuilder.length() - 1);
         queryBuilder.append(";");
         log.debug(queryBuilder.toString());
-        return queryBuilder.toString();
+        try {
+            return connection.prepareStatement(queryBuilder.toString());
+        } catch (SQLException e) {
+            log.error("invalid syntax: {}", e.getMessage());
+            throw new DataProcessingException("invalid syntax", e);
+        }
     }
 
+    @Override
     public void deleteTable(Table table) throws DatabaseConnectionException, TableMalformedException, DataProcessingException {
         try {
             final PreparedStatement statement = getDeleteStatement(getConnection(table.getDatabase()), table);
@@ -207,88 +193,5 @@ public class PostgresService extends JdbcConnector {
             log.error("invalid syntax: {}", e.getMessage());
             throw new DataProcessingException("invalid syntax", e);
         }
-    }
-
-    private String selectStatement(Table t) {
-        log.debug("selecting data from {}", t.getName());
-
-        StringBuilder queryBuilder = new StringBuilder()
-                .append("SELECT ");
-        for (TableColumn tc : t.getColumns()) {
-            queryBuilder.append(tc.getInternalName() + ",");
-        }
-        queryBuilder.deleteCharAt(queryBuilder.length() - 1);
-        queryBuilder.append(" FROM " + t.getInternalName());
-        log.debug(queryBuilder.toString());
-        return queryBuilder.toString();
-    }
-
-    /**
-     * FIXME THIS IS REMOVED IN SPRINT 2
-     *
-     * @param columnDto
-     * @return
-     */
-    private List<String> mockAnalyzeService(ColumnCreateDto[] columnDto) {
-        final List<String> columns = new LinkedList<>();
-        for (ColumnCreateDto column : columnDto) {
-            final StringBuilder columnBuilder = new StringBuilder()
-                    .append(tableMapper.columnNameToString(column.getName()))
-                    .append(" ")
-                    .append(mockDataTypeAnalyze(column.getType()))
-                    .append(mockCheckExpression(column))
-                    .append(!column.getNullAllowed() ? " NOT" : "")
-                    .append(" NULL")
-                    .append(mockForeignKey(column))
-                    .append(column.getPrimaryKey() ? " PRIMARY KEY" : "");
-            columns.add(columnBuilder.toString());
-        }
-        return columns;
-    }
-
-    /**
-     * FIXME THIS IS REMOVED IN SPRINT 2
-     *
-     * @param type
-     * @return
-     */
-    private String mockDataTypeAnalyze(ColumnTypeDto type) {
-        switch (type) {
-            case TEXT:
-            case STRING:
-                return "TEXT";
-            case NUMBER:
-                return "DOUBLE PRECISION";
-            case DATE:
-                return "TIMESTAMP";
-            case BLOB:
-                return "BYTEA";
-            case BOOLEAN:
-                return "BOOLEAN";
-            default:
-                throw new IllegalArgumentException("Unable to match datatype with PostgreSQL");
-        }
-    }
-
-    /**
-     * FIXME THIS IS REMOVED IN SPRINT 2
-     *
-     * @param column
-     * @return
-     */
-    private String mockCheckExpression(ColumnCreateDto column) {
-        if (column.getCheckExpression() == null || column.getCheckExpression().isEmpty()) {
-            return "";
-        }
-        final String columnName = tableMapper.columnNameToString(column.getName());
-        final String expression = column.getCheckExpression().replace(column.getName(), columnName);
-        return " CHECK ( " + expression + " )";
-    }
-
-    private String mockForeignKey(ColumnCreateDto column) {
-        if (column.getForeignKey() == null || column.getForeignKey().isEmpty()) {
-            return "";
-        }
-        return " FOREIGN KEY " + column.getForeignKey();
     }
 }
