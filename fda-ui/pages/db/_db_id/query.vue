948c0542e2a879d801e76aa9169339202e37946b
Kirill Stytsenko <kirill@styts.com>
2021-05-27 18:49:11 +0200

    GET db is only used in default layout: db is in store

--- fda-ui/pages/db/_db_id/query.vue
+++ fda-ui/pages/db/_db_id/query.vue
@@ -16,18 +16,12 @@ export default {
   },
   data () {
     return {
-      db: null
     }
   },
-  async mounted () {
-    try {
-      const res = await this.$axios.get(`/api/database/${this.$route.params.db_id}`)
-      this.db = res.data
-    } catch (err) {
-      this.$toast.error('Could not load database.')
+  computed: {
+    db () {
+      return this.$store.state.db
     }
-  },
-  methods: {
   }
 }
 </script>
