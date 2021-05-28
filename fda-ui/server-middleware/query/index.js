8eb60de8ff5d529cc622033a5357e5cd4b35a8f3
Kirill Stytsenko <kirill@styts.com>
2021-05-28 20:40:37 +0200

    Add casting to number
    Former-commit-id: 6b735b49a14adf959f8ecaf24ac4e1c7b75a2f6c

--- fda-ui/server-middleware/query/index.js
+++ fda-ui/server-middleware/query/index.js
@@ -11,8 +11,15 @@ export function buildQuery ({ table, select, clauses }) {
   for (let i = 0; i < clauses.length; i++) {
     const clause = clauses[i]
     const cmd = builder[clause.type]
+    let { params } = clause
+    if (!params) {
+      params = []
+    }
+    if (params.length >= 2) {
+      params[2] = castNum(params[2])
+    }
     if (typeof cmd === 'function') {
-      builder[clause.type].apply(builder, clause.params)
+      builder[clause.type].apply(builder, params)
     }
   }
 
@@ -31,4 +38,20 @@ export function buildQuery ({ table, select, clauses }) {
     sql,
     formatted
   }
+}
+
+export function castNum (s) {
+  if (typeof s !== 'string') {
+    return s
+  }
+
+  const num = Number(s)
+
+  const ss = String(num)
+
+  if (s !== ss) {
+    return s
+  }
+
+  return num
 }
