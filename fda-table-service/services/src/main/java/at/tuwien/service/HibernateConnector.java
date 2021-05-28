0299bd1e739a3742467240469adf2aa0cb69d7ab
Martin Weise <martin.weise@tuwien.ac.at>
2021-05-28 19:08:53 +0200

    specify to create database

--- fda-table-service/services/src/main/java/at/tuwien/service/HibernateConnector.java
+++ fda-table-service/services/src/main/java/at/tuwien/service/HibernateConnector.java
@@ -38,6 +38,7 @@ public class HibernateConnector implements ContainerDatabaseConnector {
         configuration.setProperty("hibernate.connection.username", ContainerDatabaseUtil.getUsername(database));
         configuration.setProperty("hibernate.connection.password", ContainerDatabaseUtil.getPassword(database));
         configuration.setProperty("hibernate.current_session_context_class", "thread");
+        configuration.setProperty("hibernate.hbm2ddl.auto", "create");
         configuration.setProperty("hibernate.show_sql", "true");
         configuration.setProperty("hibernate.format_sql", "true");
         configuration.setProperty("hibernate.mapping", "true");
