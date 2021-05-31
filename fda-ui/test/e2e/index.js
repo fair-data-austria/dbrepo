925f4c7078a5abd5c24a7ed16cd9fe6c2a4c695e
Kirill Stytsenko <kirill@styts.com>
2021-05-31 16:14:17 +0200

    e2e test 'create & delete database'
    Former-commit-id: 027bcc5a09623aa4185c21215a4cf1a9152fea69

--- fda-ui/test/e2e/index.js
+++ fda-ui/test/e2e/index.js
@@ -4,10 +4,6 @@ const { pageMacro, before, after } = require('./_utils')
 test.before(before)
 test.after(after)
 
-test('e2e placeholder', (t) => {
-  t.is(1, 1)
-})
-
 test('visit homepage', pageMacro, async (t, page) => {
   await page.go('/')
   t.is(await page.title(), 'fda-ui - fda-ui')
