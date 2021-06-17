9b3f87d857e3c3a01b45f260a0f1748d73625e92
Martin Weise <martin.weise@tuwien.ac.at>
2021-06-17 23:36:20 +0200

    added the documentation generation script

--- fda-database-service/rest-service/src/main/java/at/tuwien/config/SwaggerConfig.java
+++ fda-database-service/rest-service/src/main/java/at/tuwien/config/SwaggerConfig.java
@@ -21,7 +21,6 @@ public class SwaggerConfig {
     @Bean
     public Docket databaseApi() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .groupName("database-api")
                 .apiInfo(apiInfo())
                 .select()
                 .paths(PathSelectors.ant("/api/**"))
