0b7ce030058549d340dcda838685915571ab9883
Martin Weise <martin.weise@tuwien.ac.at>
2021-06-27 16:14:13 +0200

    somehow works in maven but not ide
    Former-commit-id: c8325c520b26028bf63da03011073e5aa06329a8

--- fda-database-service/rest-service/src/test/java/at/tuwien/service/ServiceIntegrationTest.java
+++ fda-database-service/rest-service/src/test/java/at/tuwien/service/ServiceIntegrationTest.java
@@ -1,30 +1,49 @@
 package at.tuwien.service;
 
 import at.tuwien.BaseUnitTest;
+import at.tuwien.api.database.DatabaseCreateDto;
+import at.tuwien.entities.container.image.ContainerImage;
+import at.tuwien.entities.database.Database;
+import at.tuwien.exception.ContainerNotFoundException;
+import at.tuwien.exception.ImageNotSupportedException;
+import at.tuwien.mapper.ImageMapper;
 import at.tuwien.repository.ContainerRepository;
 import at.tuwien.repository.ImageRepository;
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.command.CreateContainerResponse;
 import com.github.dockerjava.api.exception.NotModifiedException;
+import com.github.dockerjava.api.model.ExposedPort;
 import com.github.dockerjava.api.model.HostConfig;
 import com.github.dockerjava.api.model.Network;
 import com.github.dockerjava.api.model.PortBinding;
+import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
-@SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @ExtendWith(SpringExtension.class)
+@SpringBootTest
 public class ServiceIntegrationTest extends BaseUnitTest {
 
     @Autowired
@@ -38,6 +57,12 @@ public class ServiceIntegrationTest extends BaseUnitTest {
 
     @Autowired
     private ContainerRepository containerRepository;
+
+    @Autowired
+    private DatabaseService databaseService;
+
+    @Autowired
+    private ImageMapper imageMapper;
 
     private Long CONTAINER_1_ID, CONTAINER_2_ID;
 
@@ -58,9 +83,8 @@ public class ServiceIntegrationTest extends BaseUnitTest {
         imageRepository.save(IMAGE_2);
         /* create container */
         final CreateContainerResponse request = dockerClient.createContainerCmd(IMAGE_1_REPOSITORY + ":" + IMAGE_1_TAG)
-                .withEnv(IMAGE_1_ENVIRONMENT)
-                .withHostConfig(hostConfig.withNetworkMode("fda-userdb")
-                        .withPortBindings(PortBinding.parse("5433:" + IMAGE_1_PORT)))
+                .withEnv(IMAGE_1_ENV)
+                .withHostConfig(hostConfig.withNetworkMode("fda-userdb"))
                 .withName(CONTAINER_1_NAME)
                 .withIpv4Address(CONTAINER_1_IP)
                 .withHostName(CONTAINER_1_INTERNALNAME)
@@ -100,7 +124,77 @@ public class ServiceIntegrationTest extends BaseUnitTest {
                 });
     }
 
-    // create and delete database
+    @Test
+    public void findAll_empty_succeeds() {
 
-    // modify database (name)
+        /* test */
+        final List<Database> response = databaseService.findAll();
+        assertEquals(0, response.size());
+    }
+
+    @Test
+    public void create_succeeds() throws SQLException, ImageNotSupportedException, ContainerNotFoundException {
+        final DatabaseCreateDto request = DatabaseCreateDto.builder()
+                .containerId(CONTAINER_1_ID)
+                .name(DATABASE_1_NAME)
+                .isPublic(DATABASE_1_PUBLIC)
+                .build();
+
+        /* test */
+        final Database response = databaseService.create(request);
+        assertEquals(DATABASE_1_NAME, response.getName());
+        assertEquals(DATABASE_1_PUBLIC, response.getIsPublic());
+        assertEquals(CONTAINER_1_ID, response.getContainer().getId());
+    }
+
+    @Test
+    public void create_notFound_fails() {
+
+    }
+
+    @Test
+    public void create_notRunning_fails() {
+
+    }
+
+    @Test
+    public void delete_succeeds() {
+
+    }
+
+    @Test
+    public void delete_notFound_fails() {
+
+    }
+
+    @Test
+    public void delete_notRunning_fails() {
+
+    }
+
+    @Test
+    public void modify_succeeds() {
+
+    }
+
+    @Test
+    public void modify_notFound_fails() {
+
+    }
+
+    @Test
+    public void modify_notRunning_fails() {
+
+    }
+
+    @Test
+    public void find_succeeds() {
+
+    }
+
+    @Test
+    public void find_notFound_fails() {
+
+    }
+
 }
--- fda-database-service/services/src/main/java/at/tuwien/service/DatabaseService.java
+++ fda-database-service/services/src/main/java/at/tuwien/service/DatabaseService.java
@@ -1,6 +1,7 @@
 package at.tuwien.service;
 
 import at.tuwien.api.database.DatabaseCreateDto;
+import at.tuwien.api.database.DatabaseModifyDto;
 import at.tuwien.entities.container.Container;
 import at.tuwien.entities.database.Database;
 import at.tuwien.exception.*;
@@ -70,16 +71,10 @@ public class DatabaseService extends JdbcConnector {
             log.warn("Database with id {} does not exist", databaseId);
             throw new DatabaseNotFoundException("Database does not exist.");
         }
-        final Database database = databaseResponse.get();
-        // check if postgres
-        if (!database.getContainer().getImage().getRepository().equals("postgres")) {
-            log.warn("No support for {}:{}", database.getContainer().getImage().getRepository(), database.getContainer().getImage().getTag());
-            throw new ImageNotSupportedException("Currently only PostgreSQL is supported.");
-        }
-        delete(database);
+        delete(databaseResponse.get());
         databaseRepository.deleteById(databaseId);
         log.info("Deleted database {}", databaseId);
-        log.debug("deleted database {}", database);
+        log.debug("deleted database {}", databaseResponse.get());
     }
 
     @Transactional
@@ -90,23 +85,38 @@ public class DatabaseService extends JdbcConnector {
             log.warn("Container with id {} does not exist", createDto.getContainerId());
             throw new ContainerNotFoundException("Container does not exist.");
         }
-        final Container container = containerResponse.get();
-        // check if postgres
-        if (!container.getImage().getRepository().equals("postgres")) {
-            log.warn("only postgres is supported currently");
-            throw new ImageNotSupportedException("Currently only PostgreSQL is supported.");
-        }
         // call container to create database
         final Database database = new Database();
         database.setName(createDto.getName());
-        database.setContainer(container);
-        database.setIsPublic(false);
         database.setInternalName(databaseMapper.databaseToInternalDatabaseName(database));
+        database.setContainer(containerResponse.get());
+        database.setIsPublic(createDto.getIsPublic());
         create(database);
         // save in metadata database
         final Database out = databaseRepository.save(database);
         log.info("Created database {}", out.getId());
         log.debug("created database {}", out);
+        return out;
+    }
+
+    @Transactional
+    public Database modify(DatabaseModifyDto modifyDto) throws ImageNotSupportedException, SQLException,
+            DatabaseNotFoundException {
+        final Optional<Database> databaseResponse = databaseRepository.findById(modifyDto.getDatabaseId());
+        if (databaseResponse.isEmpty()) {
+            log.warn("Database with id {} does not exist", modifyDto.getDatabaseId());
+            throw new DatabaseNotFoundException("Database does not exist.");
+        }
+        // call container to create database
+        final Database database = new Database();
+        database.setName(modifyDto.getName());
+        database.setInternalName(databaseMapper.databaseToInternalDatabaseName(database));
+        database.setIsPublic(modifyDto.getIsPublic());
+        modify(database);
+        // save in metadata database
+        final Database out = databaseRepository.save(database);
+        log.info("Updated database {}", out.getId());
+        log.debug("updated database {}", out);
         return out;
     }
 
