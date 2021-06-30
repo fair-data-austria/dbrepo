03e8736134a1b6ea49d58604ce7262ad181ee30b
Martin Weise <martin.weise@tuwien.ac.at>
2021-06-30 17:48:41 +0200

    added custom hosts in user space for pipeline
    Former-commit-id: ccad302a2f720c0adfcf4279893adde68254d4c0

--- fda-ui/pages/db/_db_id/tables/index.vue
+++ fda-ui/pages/db/_db_id/tables/index.vue
@@ -3,16 +3,19 @@
     <DBToolbar v-model="db" />
     <TableList />
     <TableCreate />
+    <TableFromCSV></TableFromCSV>
   </div>
 </template>
 <script>
 import TableList from '@/components/TableList'
 import TableCreate from '@/components/TableCreate'
 import DBToolbar from '@/components/DBToolbar'
+import TableFromCSV from './table_from_csv'
 
 export default {
   name: 'Tables',
   components: {
+    TableFromCSV,
     TableList,
     TableCreate,
     DBToolbar
