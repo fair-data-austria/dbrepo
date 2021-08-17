ee8e4a14b6c8623c1376fdebdaa0e4e0ba79cfa9
Cornelia Michlits <cornelia.michlits@tuwien.ac.at>
2021-08-17 15:42:58 +0200

    84-search-service try implementation of elastic search client in db-service
    Former-commit-id: 07f18c96fb34a5967ccfe537a1377a3be9a7f71c

--- fda-database-service/rest-service/src/main/java/at/tuwien/FdaDatabaseManagingApplication.java
+++ fda-database-service/rest-service/src/main/java/at/tuwien/FdaDatabaseManagingApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -14,7 +15,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableJpaAuditing
 @EnableOpenApi
 @EnableTransactionManagement
-@EnableJpaRepositories(basePackages = {"at.tuwien.repository"})
+@EnableJpaRepositories(basePackages = {"at.tuwien.repository.jpa"})
+@EnableElasticsearchRepositories(basePackages = {"at.tuwien.repository.elasticsearch"})
 @EntityScan(basePackages = {"at.tuwien.entities"})
 public class FdaDatabaseManagingApplication {
 
--- fda-metadata-db/entities/src/main/java/at/tuwien/entities/database/Database.java
+++ fda-metadata-db/entities/src/main/java/at/tuwien/entities/database/Database.java
@@ -6,12 +6,14 @@ import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.Instant;
 import java.util.List;
 
+@Document(indexName = "databaseindex")
 @Data
 @Entity
 @Builder
