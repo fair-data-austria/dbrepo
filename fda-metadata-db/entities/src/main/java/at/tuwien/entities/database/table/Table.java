6298bbee3780a7e03d1d0c67b300d42fe9abe454
Cornelia Michlits <cornelia.michlits@tuwien.ac.at>
2021-08-27 19:19:23 +0200

    #84 create ES tblindex
    Former-commit-id: 6b4976641dfdb0f9429bb89e020ea7ed0499f709

--- fda-metadata-db/entities/src/main/java/at/tuwien/entities/database/table/Table.java
+++ fda-metadata-db/entities/src/main/java/at/tuwien/entities/database/table/Table.java
@@ -6,6 +6,7 @@ import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
@@ -17,6 +18,7 @@ import java.util.List;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Document(indexName = "tblindex", createIndex = false)
 @IdClass(TableKey.class)
 @ToString(onlyExplicitlyIncluded = true)
 @EntityListeners(AuditingEntityListener.class)
