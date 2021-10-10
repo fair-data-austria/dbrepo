66aef2f66a49da5203429c14336e99cfa0ffc6df
Martin Weise <martin.weise@tuwien.ac.at>
2021-10-10 16:46:24 +0200

    some progress
    Former-commit-id: ed854e5be127a76f876b5f90330e3cd986a2efad

--- fda-authentication-service/services/src/main/java/at/tuwien/service/AuthenticationService.java
+++ fda-authentication-service/services/src/main/java/at/tuwien/service/AuthenticationService.java
@@ -2,23 +2,18 @@ package at.tuwien.service;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.providers.ExpiringUsernameAuthenticationToken;
 import org.springframework.security.saml.SAMLAuthenticationProvider;
 import org.springframework.security.saml.SAMLCredential;
-import org.springframework.security.saml.userdetails.SAMLUserDetailsService;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.List;
 
 @Log4j2
 @Service
-public class AuthenticationService extends SAMLAuthenticationProvider implements SAMLUserDetailsService {
+public class AuthenticationService extends SAMLAuthenticationProvider {
 
     @Override
     public Collection<? extends GrantedAuthority> getEntitlements(SAMLCredential credential, Object userDetail) {
@@ -28,15 +23,5 @@ public class AuthenticationService extends SAMLAuthenticationProvider implements
         } else {
             return Collections.emptyList();
         }
-    }
-
-    @Override
-    public Object loadUserBySAML(SAMLCredential credential) throws UsernameNotFoundException {
-        final String userID = credential.getNameID().getValue();
-        log.debug("Logged in user {}", userID);
-        List<GrantedAuthority> authorities = new ArrayList<>();
-        GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_USER");
-        authorities.add(authority);
-        return new User(userID, "<abc123>", true, true, true, true, authorities);
     }
 }
