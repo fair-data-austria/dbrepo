1836ea1b5551873b85f119cd5e2db6423e744fe4
Moritz Staudinger <moritz.staudinger@tuwien.ac.at>
2021-11-11 02:09:01 +0100

    Removed notnull values and added default values for timestamp and paging
    Former-commit-id: ef1b131bc83a32141dcd8aeab8b7cb100a484c14

--- fda-table-service/services/src/main/java/at/tuwien/service/impl/MariaDataService.java
+++ fda-table-service/services/src/main/java/at/tuwien/service/impl/MariaDataService.java
@@ -172,9 +172,18 @@ public class MariaDataService extends JdbcConnector implements DataService {
 
     @Override
     @Transactional
-    public QueryResultDto selectAll(@NonNull Long databaseId, @NonNull Long tableId, @NonNull Instant timestamp,
-                                    @NonNull Long page, @NonNull Long size) throws TableNotFoundException,
+    public QueryResultDto selectAll(@NonNull Long databaseId, @NonNull Long tableId, Instant timestamp,
+                                    Long page, Long size) throws TableNotFoundException,
             DatabaseNotFoundException, ImageNotSupportedException, DatabaseConnectionException, TableMalformedException {
+        if(timestamp == null) {
+            timestamp = Instant.now();
+        }
+        if(page == null ) {
+            page = 0L;
+        }
+        if(size == null) {
+            size = 100L;
+        }
         if (page < 0) {
             throw new TableMalformedException("Page number cannot be lower than 0");
         }
