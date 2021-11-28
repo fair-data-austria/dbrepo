86bc6c99e1ffe3d49326a7eacd59214384a8143f
Martin Weise <martin.weise@tuwien.ac.at>
2021-11-28 21:34:15 +0100

    Add default keystore, fixed gateway for authentication

--- fda-authentication-service/rest-service/src/main/java/at/tuwien/config/ReadyConfig.java
+++ fda-authentication-service/rest-service/src/main/java/at/tuwien/config/ReadyConfig.java
@@ -1,6 +1,7 @@
 package at.tuwien.config;
 
 import com.google.common.io.Files;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +10,7 @@ import org.springframework.context.event.EventListener;
 import java.io.File;
 import java.io.IOException;
 
+@Log4j2
 @Configuration
 public class ReadyConfig {
 
@@ -17,6 +19,7 @@ public class ReadyConfig {
 
     @EventListener(ApplicationReadyEvent.class)
     public void init() throws IOException {
+        log.debug("Service ready");
         Files.touch(new File(readyPath));
     }
 
