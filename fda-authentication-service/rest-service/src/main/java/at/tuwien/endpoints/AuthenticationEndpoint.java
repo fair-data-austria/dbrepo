becd1c44f9ca722e705035c367c82b238502e6e2
Martin Weise <martin.weise@tuwien.ac.at>
2021-12-09 13:02:20 +0100

    Api

--- fda-authentication-service/rest-service/src/main/java/at/tuwien/endpoints/AuthenticationEndpoint.java
+++ fda-authentication-service/rest-service/src/main/java/at/tuwien/endpoints/AuthenticationEndpoint.java
@@ -1,23 +1,33 @@
 package at.tuwien.endpoints;
 
+import at.tuwien.config.FdaConfig;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Log4j2
 @RestController
 @CrossOrigin(origins = "*")
 public class AuthenticationEndpoint {
 
-    @GetMapping("/api/auth")
-    public String index() {
-        return "Index";
+    private final FdaConfig fdaConfig;
+
+    @Autowired
+    public AuthenticationEndpoint(FdaConfig fdaConfig) {
+        this.fdaConfig = fdaConfig;
     }
 
-    @GetMapping("/api/auth/info")
-    public ResponseEntity<Object> info(Authentication authentication) {
-        return ResponseEntity.ok(authentication.getName());
+    @GetMapping("/")
+    public void authenticated(HttpServletResponse response, Authentication authentication) throws IOException {
+        if (authentication.isAuthenticated()) {
+            response.sendRedirect(fdaConfig.getBaseUrl());
+        }
     }
 
 }
---
+++ fda-authentication-service/rest-service/src/main/java/at/tuwien/endpoints/UserEndpoint.java
@@ -0,0 +1,23 @@
+package at.tuwien.endpoints;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@Log4j2
+@RestController
+@CrossOrigin(origins = "*")
+public class UserEndpoint {
+
+    @GetMapping("/api/auth")
+    public String index() {
+        return "Index";
+    }
+
+    @GetMapping("/api/auth/info")
+    public ResponseEntity<Object> info(Authentication authentication) {
+        return ResponseEntity.ok(authentication.getName());
+    }
+
+}
