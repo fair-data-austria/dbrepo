f954fd7ed45b6937c2ce9286ec8bb2e17836b7de
Martin Weise <martin.weise@tuwien.ac.at>
2021-12-12 20:20:06 +0100

    named params
    Former-commit-id: bb1b51b7665fbc5e8406affff0fbb111fbd9fbfe

--- fda-authentication-service/services/src/main/java/at/tuwien/repositories/UserRepository.java
+++ fda-authentication-service/services/src/main/java/at/tuwien/repositories/UserRepository.java
@@ -2,11 +2,14 @@ package at.tuwien.repositories;
 
 import at.tuwien.entities.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByOId(Long oId);
+    @Query(value = "select u from User u where u.oId = :oid")
+    Optional<User> findByOId(@Param("oid") Long oId);
 
 }
