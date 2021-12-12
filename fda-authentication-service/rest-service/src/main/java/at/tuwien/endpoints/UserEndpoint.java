7bf027762e57738ecf1f73dbebeab7b79aa101ea
Martin Weise <martin.weise@tuwien.ac.at>
2021-12-12 21:28:34 +0100

    need a responseentity
    Former-commit-id: 83cf9ffd4ec3bc87504cd611d847d2bc22214f3f

--- fda-authentication-service/rest-service/src/main/java/at/tuwien/endpoints/UserEndpoint.java
+++ fda-authentication-service/rest-service/src/main/java/at/tuwien/endpoints/UserEndpoint.java
@@ -6,6 +6,7 @@ import at.tuwien.mapper.UserMapper;
 import at.tuwien.service.UserService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,17 +26,19 @@ public class UserEndpoint {
         this.userService = userService;
     }
 
-    @GetMapping
-    public List<UserDto> findAll() {
-        return userService.findAll()
+    @GetMapping("/")
+    public ResponseEntity<List<UserDto>> findAll() {
+        final List<UserDto> users = userService.findAll()
                 .stream()
                 .map(userMapper::userToUserDto)
                 .collect(Collectors.toList());
+        return ResponseEntity.ok(users);
     }
 
     @GetMapping("/{id}")
-    public UserDto findById(@PathVariable("id") Long id) throws UserNotFoundException {
-        return userMapper.userToUserDto(userService.findById(id));
+    public ResponseEntity<UserDto> findById(@PathVariable("id") Long id) throws UserNotFoundException {
+        final UserDto user = userMapper.userToUserDto(userService.findById(id));
+        return ResponseEntity.ok(user);
     }
 
 }
