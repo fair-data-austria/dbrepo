14e0796e06f3e68d5f6971a4d3c498fd2243d3d2
Martin Weise <martin.weise@tuwien.ac.at>
2021-12-12 18:24:39 +0100

    Debug
    Former-commit-id: 4af291500a0374f2e50aeb0f62d93d8a1e21c422

--- fda-authentication-service/services/src/main/java/at/tuwien/service/impl/AuthenticationServiceImpl.java
+++ fda-authentication-service/services/src/main/java/at/tuwien/service/impl/AuthenticationServiceImpl.java
@@ -2,6 +2,7 @@ package at.tuwien.service.impl;
 
 import lombok.extern.log4j.Log4j2;
 import org.opensaml.saml2.core.Attribute;
+import org.opensaml.xml.security.credential.Credential;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -12,6 +13,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Log4j2
 @Service
@@ -21,10 +24,20 @@ public class AuthenticationServiceImpl implements SAMLUserDetailsService {
     public Object loadUserBySAML(SAMLCredential credential) throws UsernameNotFoundException {
         final List<Attribute> attributes = credential.getAttributes();
         final String id = credential.getNameID().getValue();
-        log.debug("user detail id {}, other attributes {}", id, attributes);
+        log.debug("user detail id {}", id);
+        printAttribute(credential, "oid");
+        printAttribute(credential, "sn");
+        printAttribute(credential, "givenName");
+        printAttribute(credential, "mail");
         final List<GrantedAuthority> authorities = new ArrayList<>();
         final GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_USER");
         authorities.add(authority);
         return new User(id, "", authorities);
+    }
+
+    protected void printAttribute(SAMLCredential credential, String name) {
+        if (credential.getAttribute(name) != null) {
+            log.debug("Name {}, Value {}", name, credential.getAttributeAsString(name));
+        }
     }
 }
