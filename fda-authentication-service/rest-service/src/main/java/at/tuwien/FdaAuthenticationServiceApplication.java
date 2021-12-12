f3716ebdbbaa817718d1eb55e8808b899c9a2382
Martin Weise <martin.weise@tuwien.ac.at>
2021-12-12 20:46:17 +0100

    forgot the jpa auditing annotation
    Former-commit-id: 2c0dc533c1ac930b225a89cfb965d797454baa70

--- fda-authentication-service/rest-service/src/main/java/at/tuwien/FdaAuthenticationServiceApplication.java
+++ fda-authentication-service/rest-service/src/main/java/at/tuwien/FdaAuthenticationServiceApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import springfox.documentation.oas.annotations.EnableOpenApi;
 
 @EnableOpenApi
 @EnableWebSecurity
 @SpringBootApplication
+@EnableJpaAuditing
 public class FdaAuthenticationServiceApplication {
 
     public static void main(String[] args) {
