2993534fef8ca73f126d4f345d0cee4a33267c65
Moritz Staudinger <moritz.staudinger@tuwien.ac.at>
2022-02-14 13:38:27 +0100

    Fixed Seeder Calls @mweise
    Former-commit-id: 2184d8fd5cfdd50cc6a42b949cd9056697b2d709

--- fda-container-service/services/src/main/java/at/tuwien/seeder/Seeder.java
+++ fda-container-service/services/src/main/java/at/tuwien/seeder/Seeder.java
@@ -1,10 +1,6 @@
 package at.tuwien.seeder;
 
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
-
 public interface Seeder {
 
-    @EventListener(ApplicationReadyEvent.class)
     void seed();
 }
